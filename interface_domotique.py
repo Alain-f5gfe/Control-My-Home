c2f2d9567ec005971b64e6dca11bcb712dee28bc
f5gfe <f5gfe@wanadoo.fr>
2022-06-08 17:39:35 +0200

    message d'erreurs

--- interface_domotique.py
+++ interface_domotique.py
@@ -1,4 +1,5 @@
-#!/home/f5gfe/tableau_de_bord/.venv/bin/python3
+#!/usr/bin/python3.8
+#/home/f5gfe/tableau_de_bord/.venv/bin/python3
 # -*- encoding: utf-8 -*-
 
 ''' 
@@ -18,7 +19,7 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt
 from pathlib import Path
 import etat_sonoff
-from etat_sonoff import sonoffIP, send_ordre_on, send_ordre_off
+from etat_sonoff import sonoffIP, send_ordre_on, send_ordre_off, lecture_mesure
 
 class MainWindow(QMainWindow):
 
@@ -39,10 +40,10 @@ class MainWindow(QMainWindow):
         for inter in etat_sonoff.sonoffIP:
             self.etat_actuel = (sonoffIP.get(inter)[1])
             item = OnOffWidget(inter)
-            print(inter, item)
+            #print(inter, item)
             self.controlsLayout.addWidget(item)
             self.widgets.append(item)
-            print(self.widgets)
+            #print(self.widgets)
         
         
         # mise en page des éléments gestion de la taille des widgets
@@ -66,23 +67,58 @@ class MainWindow(QMainWindow):
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
         self.searchbar.setCompleter(self.completer)
 
+        # Ajouter des lignes info
+        va = []
+        self.alim = QLabel()
+        try:
+            va = lecture_mesure("192.168.1.220")
+            #self.alim.setText(str(va))
+            self.alim.setText(str(va[0])+"="+str(va[1])+"V   Courant="+str(va[3])+"A")
+        except:
+            self.alim.setText("Erreur de connexion")
+            pass
+
+        self.meteo = QLabel()
+        tps = []
+        try:
+            tps = lecture_mesure("192.168.1.110")
+            #self.meteo.setText(str(temps))
+            self.meteo.setText(str(tps[0])+"="+str(tps[1])+"°   "\
+                +str(tps[2])+"="+str(tps[3])+"%    "\
+                +str(tps[4])+"="+str(tps[5])+"Hp")
+        except:
+            self.meteo.setText("Erreur de connexion")
+            pass
+
+        self.congel = QLabel()
+        froid =[]
+        try:
+            froid = lecture_mesure("192.168.1.13", "5")# je vise le 5eme bloc de données
+            self.congel.setText(str(froid[0])+" congélateur = "+ str(froid[1])+"°")
+        except:
+            self.congel.setText("Erreur de connexion")
+            pass
+
+
         # Ajouter la ligne recherche VBoxLayout (appliqué au widget principal
         # qui englobe l'ensemble de la fenêtre).
         main = QWidget()
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.searchbar)
+        mainLayout.addWidget(self.alim)
+        mainLayout.addWidget(self.meteo)
+        mainLayout.addWidget(self.congel)
         mainLayout.addWidget(self.scroll)
 
         main.setLayout(mainLayout)
         self.setCentralWidget(main)
-
-        self.setGeometry(700, 100, 800, 700)
+        self.setGeometry(500, 100, 500, 700)
         self.setWindowTitle('Contrôle de la domotique')
 
     def update_display(self, text):
         '''
-        rafraichi l'affichage. + comparer le texte nom des zones aux mots 
-        de barre de recherche après conversion en minuscule
+        rafraichit l'affichage de barre de recherche en 
+        fonction de la recherche après conversion en minuscule
         '''
         for widget in self.widgets:
             if text.lower() in widget.name.lower():
@@ -97,7 +133,7 @@ class MainWindow(QMainWindow):
         
     def persit():
         ''' sauve l état des widgets '''
-        print("liste widgets ", self.widgets)
+        # print("liste widgets ", self.widgets)
 
 
 class OnOffWidget(QWidget):
@@ -108,7 +144,7 @@ class OnOffWidget(QWidget):
         super(OnOffWidget, self).__init__()
 
         self.is_on = 0
-        print("le nom du widget ", name)
+        # print("le nom du widget ", name)
         self.name = name
         self.etat_actuel = (sonoffIP.get(self.name)[1])
         self.lbl = QLabel(self.name)
@@ -122,8 +158,6 @@ class OnOffWidget(QWidget):
         self.hbox.addWidget(self.btn_on)
         self.hbox.addWidget(self.btn_off)
 
-        # récupérer le non de la ligne ????
-        
         self.btn_on.clicked.connect(self.on)
         self.btn_off.clicked.connect(self.off)
         self.setLayout(self.hbox)
@@ -133,15 +167,15 @@ class OnOffWidget(QWidget):
         """
         dans la recherche afficher ces widgets, et tous les widgets enfants.
         """
-        for w in [self, self.lbl, self.btn_on, self.btn_off]:
-            w.setVisible(True)
+        for bidule in [self, self.lbl, self.btn_on, self.btn_off]:
+            bidule.setVisible(True)
 
     def hide(self):
         """
         cacher ces widgets, et tous les widgets enfants.
         """
-        for w in [self, self.lbl, self.btn_on, self.btn_off]:
-            w.setVisible(False)
+        for bidule in [self, self.lbl, self.btn_on, self.btn_off]:
+            bidule.setVisible(False)
 
     def off(self):
         self.etat_actuel = 0
@@ -162,9 +196,9 @@ class OnOffWidget(QWidget):
         """
         if self.etat_actuel == 1:
             self.btn_on.setStyleSheet("background-color: #008000; color: #fff;")
-            self.btn_off.setStyleSheet("background-color: none; color: none;")
+            self.btn_off.setStyleSheet("background-color: none; color: #fff;")
         else:
-            self.btn_on.setStyleSheet("background-color: none; color: none;")
+            self.btn_on.setStyleSheet("background-color: none; color: #fff;")
             self.btn_off.setStyleSheet("background-color: #FF0000; color: #fff;")
             
     def persit(widgets):
@@ -173,7 +207,7 @@ class OnOffWidget(QWidget):
         
 
 app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
+window = MainWindow()
+window.show()
 sys.exit(app.exec())
 
